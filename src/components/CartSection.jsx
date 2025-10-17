23b0a97e32f57dd8d0d3c113adb14d4fc7dd63aa
lesley_w <lrwesley92@gmail.com>
2025-10-16 20:44:32 -0500

    Added FullCart component

--- src/components/CartSection.jsx
+++ src/components/CartSection.jsx
@@ -1,11 +1,12 @@
 import EmptyCart from "./EmptyCart"
+import FullCart from "./FullCart"
 
 export default function CartSection() {
   return (
     <div className="cart">
       <div className="cart-container">
         <h2>Your Cart (0)</h2>
-        <EmptyCart />
+        <FullCart />
       </div>
     </div>
   )
---
+++ src/components/FullCart.jsx
@@ -0,0 +1,5 @@
+export default function FullCart() {
+  return (
+    <h3>This is where the full cart goes!</h3>
+  )
+}
