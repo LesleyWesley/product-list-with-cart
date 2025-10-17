46f389f894214ca4ee4c37679158d09c0055705c
lesley_w <lrwesley92@gmail.com>
2025-10-17 16:12:15 -0500

    Added jsx for FullCart component

--- src/components/FullCart.jsx
+++ src/components/FullCart.jsx
@@ -1,5 +1,21 @@
 export default function FullCart() {
   return (
-    <h3>This is where the full cart goes!</h3>
+    <div className="full-cart">
+      <ul>
+        //Cart items will go here
+      </ul>
+
+      <div className="total-section">
+        <p>Order Total</p>
+        <span className="order-total">$46.50</span>
+      </div>
+
+      <div className="eco-section">
+        <img src="./src/assets/icon-carbon-neutral.svg" alt="" />
+        <p>This is a <span>carbon-neutral</span> delivery</p>
+      </div>
+
+      <button className="confirm-order">Confirm Order</button>
+    </div>
   )
 }
