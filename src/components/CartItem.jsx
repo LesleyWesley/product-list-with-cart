ff2babf7c30d841eb30f6adcb0f48bafe948ad38
lesley_w <lrwesley92@gmail.com>
2025-10-18 16:01:27 -0500

    Added mobile styoling to CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,15 +2,15 @@ export default function CartItem() {
   return (
     <li className="cart-item">
       <div className="cart-item-left">
-        <h3>Vanilla Bean Creme Brulee</h3>
+        <h3>Classic Tiramisu</h3>
         <div className="cart-item-details">
-            <span className="item-amount">4x</span>
-            <span className="single-price">@ $7.00</span>
-            <span className="cart-item-total">$28.00</span>
+            <span className="item-amount">1x</span>
+            <span className="single-price">@ $5.50</span>
+            <span className="cart-item-total">$5.50</span>
         </div>
       </div>
 
-      <button aria-label="Delete from cart">
+      <button aria-label="Delete from cart" className="delete-button">
         <img src="./src/assets/icon-remove-item.svg" alt="Remove item" />
       </button>
     </li>
