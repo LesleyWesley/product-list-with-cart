eebe32cfdd6d16692373790a12ed02417d72a382
lesley_w <lrwesley92@gmail.com>
2025-11-16 20:07:27 -0600

    Started from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProductSection from "./components/ProductSection"
-import CartSection from "./components/CartSection"
+
 
 function App() {
 
-  const [cart, setCart] = React.useState([]);
-
   return (
-    <main>
-      <ProductSection cart = {cart} setCart={setCart}/>
-      <CartSection cart={cart}/>
-    </main>
+    <h1>Hello World!</h1>
   )
 }
 
